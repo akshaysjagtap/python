e99fc03c488d8fc1c411676f44dcb0560512c1e1
akshaysjagtap <121491417+akshaysjagtap@users.noreply.github.com>
2024-04-04 16:05:29 +0530

    Update offline.py

--- offline.py
+++ offline.py
@@ -1,26 +1,67 @@
 import os, sys
 
-print("Offline downloading packages for "+sys.argv[1]+"...\n")
-os.system("apt-rdepends "+sys.argv[1]+" -v 2> /dev/null > list.txt")
+setMainPkg=set()
+print("\nOffline downloading packages for "+sys.argv[1]+"...\n")
 
-if os.path.exists("list.txt") == True:
-	filePkgList = open("list.txt", "r")
+def rdepends():
 	setPkgs = set()
 	
-	for line in filePkgList:
-		line=line.strip()
-		
-		if line.startswith("node: "): 
-			setPkgs.add(line.split("\"")[1])
-		elif line.startswith("edge: "):
-			setPkgs.add(line.split("\"")[3])
+	os.system("apt-rdepends "+sys.argv[1]+" -v 2> /dev/null > list.txt")
 	
-	setPkgs=sorted(setPkgs)
-	
-	for pkg in setPkgs:
-		print("Downloading package: "+pkg+"...")
-		os.system("apt download " + pkg + " 2> /dev/null")
-		print()
+	if os.path.exists("list.txt") == True:
+		filePkgList = open("list.txt", "r")
 		
-	os.system("mkdir -p debpkgs")
-	os.system("mv *.deb debpkgs")	
+		for line in filePkgList:
+			line=line.strip()
+			
+			if line.startswith("node: "): 
+				setPkgs.add(line.split("\"")[1])
+			elif line.startswith("edge: "):
+				setPkgs.add(line.split("\"")[3])
+				
+		filePkgList.close()
+	
+	return setPkgs
+		
+def funcPkgList(pkgName):
+	os.system("apt-cache depends "+pkgName+" > list.txt")
+	
+	setPkgs = set()
+	
+	if os.path.exists("list.txt") == True:
+		filePkgList = open("list.txt", "r")
+		
+		for line in filePkgList:
+			line=line.strip()
+			
+			if line.startswith("Depends: "):
+				pos = line.find(" ") 
+				if pos > -1:
+					line=line[pos+1:]
+					if line[0] != "<":
+						setPkgs.add(line)
+		
+		filePkgList.close()
+	
+	return setPkgs
+
+setTemp=set()
+setTemp=setMainPkg=funcPkgList(sys.argv[1])
+	
+while True:
+	setDummy=set()
+
+	for setMem in setTemp:
+		setDummy.update(funcPkgList(setMem))
+	
+	if (len(setDummy) == 0) or (setTemp == setDummy):
+		break
+	else:
+		setMainPkg.update(setDummy)
+		setTemp = setDummy
+
+setMainPkg.update(rdepends())
+
+for pkg in setMainPkg:
+	print("Downloading package: "+pkg+"...")
+	os.system("apt download " + pkg + " 2> /dev/null")		
